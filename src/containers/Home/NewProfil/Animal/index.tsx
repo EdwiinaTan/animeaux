0e34ff049580a4b42b7e93c29277625e2e8f7ab3
Edwina <edwina.tan@hotmail.fr>
2023-04-10 16:06:43 +0200

    [10] - Add react native step indicator

--- src/containers/Home/NewProfil/Animal/index.tsx
+++ src/containers/Home/NewProfil/Animal/index.tsx
@@ -1,7 +1,11 @@
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 import { Text } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import StepIndicator from 'react-native-step-indicator'
 import { HeaderComponent } from 'src/components/Header'
 import { Layout } from 'src/components/Layout'
+import { theme } from 'src/constant/Theme'
 
 export const AddAnimal = () => {
   const navigation = useNavigation()
@@ -9,10 +13,53 @@ export const AddAnimal = () => {
   const onClickGoBack = () => {
     return navigation.goBack()
   }
+
+  const labels = ['Profil', 'Situation', 'Photos']
+
+  const customStyles = {
+    stepIndicatorSize: 25,
+    currentStepIndicatorSize: 30,
+    separatorStrokeWidth: 2,
+    currentStepStrokeWidth: 3,
+    stepStrokeCurrentColor: theme.lightColors.primary,
+    stepStrokeWidth: 3,
+    stepStrokeFinishedColor: theme.lightColors.primary,
+    stepStrokeUnFinishedColor: '#aaaaaa',
+    separatorFinishedColor: theme.lightColors.primary,
+    separatorUnFinishedColor: '#aaaaaa',
+    stepIndicatorFinishedColor: theme.lightColors.primary,
+    stepIndicatorUnFinishedColor: '#ffffff',
+    stepIndicatorCurrentColor: '#ffffff',
+    stepIndicatorLabelFontSize: 13,
+    currentStepIndicatorLabelFontSize: 13,
+    stepIndicatorLabelCurrentColor: theme.lightColors.primary,
+    stepIndicatorLabelFinishedColor: '#ffffff',
+    stepIndicatorLabelUnFinishedColor: '#aaaaaa',
+    labelColor: '#999999',
+    labelSize: 13,
+    currentStepLabelColor: theme.lightColors.primary,
+  }
+
+  const [currentPosition, setCurrentPosition] = useState(0)
+
+  const onPageChange = () => {
+    setCurrentPosition(currentPosition + 1)
+  }
+
   return (
     <Layout>
       <HeaderComponent onClickGoBack={onClickGoBack} title="Ajouter un animal" />
-      <Text>ADD ANIMAL</Text>
+
+      <StepIndicator
+        customStyles={customStyles}
+        currentPosition={currentPosition}
+        labels={labels}
+        stepCount={3}
+        direction="horizontal"
+      />
+      <TouchableOpacity onPress={onPageChange}>
+        <Text>NEXT</Text>
+      </TouchableOpacity>
     </Layout>
   )
 }

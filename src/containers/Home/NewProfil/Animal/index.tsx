01a9c1b7dd7e65bfe03e19e6408bcbe6fed7a604
Edwina <edwina.tan@hotmail.fr>
2023-04-10 16:22:01 +0200

    [10] - Add form profil animal

--- src/containers/Home/NewProfil/Animal/index.tsx
+++ src/containers/Home/NewProfil/Animal/index.tsx
@@ -1,12 +1,18 @@
 import { useNavigation } from '@react-navigation/native'
+import { Field, Formik } from 'formik'
 import { useState } from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { SelectList } from 'react-native-dropdown-select-list'
+import { Button, Divider } from 'react-native-elements'
 import { TouchableOpacity } from 'react-native-gesture-handler'
 import StepIndicator from 'react-native-step-indicator'
 import { HeaderComponent } from 'src/components/Header'
 import { Layout } from 'src/components/Layout'
 import { Spacing } from 'src/components/Layout/Spacing'
 import { theme } from 'src/constant/Theme'
+import { colorArray, genderArray, raceArray, specieArray } from 'src/utils/Animal'
+import { CheckBoxComponent } from '../../Animal/Update/Checkbox'
+import { Card, Container } from './Styled'
 
 export const AddAnimal = () => {
   const navigation = useNavigation()
@@ -51,6 +57,17 @@ export const AddAnimal = () => {
     }
   }
 
+  const initialValues = {
+    species: '',
+    gender: '',
+    name: '',
+    alias: '',
+    icadNumber: '',
+    race: '',
+    color: '',
+    publicDescription: '',
+  }
+
   return (
     <Layout>
       <HeaderComponent onClickGoBack={onClickGoBack} title="Ajouter un animal" />
@@ -62,6 +79,154 @@ export const AddAnimal = () => {
         stepCount={3}
         direction="horizontal"
       />
+
+      <Container>
+        <Card>
+          <Formik
+            initialValues={initialValues}
+            onSubmit={(values) => {
+              console.log('value', values)
+            }}
+          >
+            {({ handleChange, values, handleSubmit, handleBlur }) => (
+              <>
+                <View style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
+                  <Text>Espèce</Text>
+                  <Spacing size="8" />
+                  {specieArray.map((specie, key) => (
+                    <CheckBoxComponent
+                      key={`species_${key}`}
+                      animal={specie}
+                      values={values.species}
+                      handleChange={() => handleChange('species')(specie.value)}
+                    />
+                  ))}
+                  <Spacing size="24" />
+                  <Text>Genre</Text>
+                  <Spacing size="8" />
+                  {genderArray.map((gender, key) => (
+                    <CheckBoxComponent
+                      key={`gender_${key}`}
+                      animal={gender}
+                      values={values.gender}
+                      handleChange={() => handleChange('gender')(gender.value)}
+                    />
+                  ))}
+                </View>
+                <Spacing size="24" />
+                <Divider />
+                <Spacing size="8" />
+                <Text>Informations</Text>
+                <Spacing size="8" />
+                <Text style={{ fontSize: 15, marginBottom: 5 }}>Nom</Text>
+                <Field name="name">
+                  {({ field }) => (
+                    <TextInput
+                      {...field}
+                      style={styles.input}
+                      placeholder="Veuillez mettre le nom de l’animal"
+                      onChangeText={handleChange('name')}
+                      onChange={handleChange('name')}
+                      onBlur={handleBlur('name')}
+                      value={values.name}
+                    />
+                  )}
+                </Field>
+                <Text style={{ fontSize: 15, marginBottom: 5 }}>Alias</Text>
+                <Field name="alias">
+                  {({ field }) => (
+                    <TextInput
+                      focusable
+                      {...field}
+                      // autoFocus
+                      style={styles.input}
+                      placeholder="Veuillez mettre l’alias"
+                      onChangeText={handleChange('alias')}
+                      onChange={handleChange('alias')}
+                      onBlur={handleBlur('alias')}
+                      value={values.alias}
+                    />
+                  )}
+                </Field>
+                <Text style={{ fontSize: 15, marginBottom: 5 }}>Icad</Text>
+                <Field name="icadNumber">
+                  {({ field }) => (
+                    <TextInput
+                      {...field}
+                      style={styles.input}
+                      placeholder="Veuillez mettre le numéro icad"
+                      onChangeText={handleChange('icadNumber')}
+                      onChange={handleChange('icadNumber')}
+                      onBlur={handleBlur('icadNumber')}
+                      value={values.icadNumber}
+                    />
+                  )}
+                </Field>
+                <Text style={{ fontSize: 15, marginBottom: 5 }}>Race</Text>
+                <Spacing size="4" />
+                <View style={{ width: '100%' }}>
+                  <Field name="race">
+                    {({ field }) => (
+                      <SelectList
+                        inputStyles={{ padding: 0 }}
+                        boxStyles={{ width: '100%', borderColor: theme.lightColors.greyOutline }}
+                        {...field}
+                        searchPlaceholder="Rechercher"
+                        setSelected={handleChange('race')}
+                        onChange={handleChange('race')}
+                        data={raceArray}
+                        placeholder="Veuillez choisir la race"
+                        save="value"
+                        value={values.race}
+                      />
+                    )}
+                  </Field>
+                </View>
+                <Spacing size="16" />
+                <Text style={{ fontSize: 15, marginBottom: 5 }}>Couleur</Text>
+                <Spacing size="4" />
+                <View style={{ width: '100%' }}>
+                  <Field name="color">
+                    {({ field }) => (
+                      <SelectList
+                        boxStyles={{ width: '100%', borderColor: theme.lightColors.greyOutline }}
+                        {...field}
+                        searchPlaceholder="Rechercher"
+                        setSelected={handleChange('color')}
+                        onChange={handleChange('color')}
+                        data={colorArray}
+                        placeholder="Veuillez choisir la couleur"
+                        save="value"
+                        value={values.color}
+                      />
+                    )}
+                  </Field>
+                </View>
+                <Spacing size="16" />
+                <Text style={{ fontSize: 15, marginBottom: 5 }}>Description public</Text>
+                <Field name="publicDescription">
+                  {({ field }) => (
+                    <TextInput
+                      focusable
+                      {...field}
+                      editable
+                      multiline
+                      style={styles.input}
+                      onChangeText={handleChange('publicDescription')}
+                      onChange={handleChange('publicDescription')}
+                      onBlur={handleBlur('publicDescription')}
+                      value={values.publicDescription}
+                    />
+                  )}
+                </Field>
+                <Spacing size="24" />
+                <Button title="Submit" onPress={() => handleSubmit()} />
+              </>
+            )}
+          </Formik>
+        </Card>
+        <Spacing size="24" />
+      </Container>
       {currentPosition !== 0 && (
         <TouchableOpacity onPress={() => onPageChange('prev')}>
           <Text>Précédent</Text>
@@ -74,3 +239,15 @@ export const AddAnimal = () => {
     </Layout>
   )
 }
+
+const styles = StyleSheet.create({
+  input: {
+    width: '100%',
+    padding: 10,
+    marginBottom: 10,
+    backgroundColor: theme.lightColors.white,
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: theme.lightColors.greyOutline,
+  },
+})
--- src/utils/Animal/index.tsx
+++ src/utils/Animal/index.tsx
@@ -1,4 +1,10 @@
-import { AnimalReasonEnum } from 'src/types/Animal/enum'
+import {
+  AnimalColorEnum,
+  AnimalGenderEnum,
+  AnimalRaceEnum,
+  AnimalReasonEnum,
+  AnimalTypeEnum,
+} from 'src/types/Animal/enum'
 
 export const renderReason = (reason: AnimalReasonEnum) => {
   switch (reason) {
@@ -14,3 +20,66 @@ export const renderReason = (reason: AnimalReasonEnum) => {
       return 'Maltraitance'
   }
 }
+
+export const specieArray = [
+  { label: 'Chien', value: AnimalTypeEnum.DOG },
+  { label: 'Chat', value: AnimalTypeEnum.CAT },
+  { label: 'Rongeur', value: AnimalTypeEnum.RONDENT },
+  { label: 'Oiseau', value: AnimalTypeEnum.BIRD },
+  { label: 'Reptile', value: AnimalTypeEnum.REPTILE },
+]
+
+export const genderArray = [
+  { label: 'Male', value: AnimalGenderEnum.MALE },
+  { label: 'Femelle', value: AnimalGenderEnum.FEMALE },
+]
+
+export const raceArray = [
+  { key: AnimalRaceEnum.BEAGLE, value: AnimalRaceEnum.BEAGLE },
+  { key: AnimalRaceEnum.BERGER_ALLEMAND, value: AnimalRaceEnum.BERGER_ALLEMAND },
+  { key: AnimalRaceEnum.BERGER_SERBE, value: AnimalRaceEnum.BERGER_SERBE },
+  { key: AnimalRaceEnum.BORDER_COLLIE, value: AnimalRaceEnum.BORDER_COLLIE },
+  { key: AnimalRaceEnum.BOULEDOGUE_ANGLAIS, value: AnimalRaceEnum.BOULEDOGUE_ANGLAIS },
+  { key: AnimalRaceEnum.BOULEDOGUE_FRANCAIS, value: AnimalRaceEnum.BOULEDOGUE_FRANCAIS },
+  { key: AnimalRaceEnum.BOWER, value: AnimalRaceEnum.BOWER },
+  { key: AnimalRaceEnum.CANICHE, value: AnimalRaceEnum.CANICHE },
+  { key: AnimalRaceEnum.COCHON_INDE, value: AnimalRaceEnum.COCHON_INDE },
+  { key: AnimalRaceEnum.CROISE, value: AnimalRaceEnum.CROISE },
+  { key: AnimalRaceEnum.DOGUE_ARGENTIN, value: AnimalRaceEnum.DOGUE_ARGENTIN },
+  { key: AnimalRaceEnum.EUROPEEN, value: AnimalRaceEnum.EUROPEEN },
+  { key: AnimalRaceEnum.GRIFFON, value: AnimalRaceEnum.GRIFFON },
+  { key: AnimalRaceEnum.JACK_RUSSEL, value: AnimalRaceEnum.JACK_RUSSEL },
+  { key: AnimalRaceEnum.LAPIN, value: AnimalRaceEnum.LAPIN },
+  { key: AnimalRaceEnum.MALINOIS, value: AnimalRaceEnum.MALINOIS },
+  { key: AnimalRaceEnum.PEKINOIS, value: AnimalRaceEnum.PEKINOIS },
+  { key: AnimalRaceEnum.PINSCHER, value: AnimalRaceEnum.PINSCHER },
+  { key: AnimalRaceEnum.ROTTWEILER, value: AnimalRaceEnum.ROTTWEILER },
+  { key: AnimalRaceEnum.SIAMOIS, value: AnimalRaceEnum.SIAMOIS },
+  { key: AnimalRaceEnum.STAFF, value: AnimalRaceEnum.STAFF },
+  { key: AnimalRaceEnum.TECKEL, value: AnimalRaceEnum.TECKEL },
+]
+
+export const colorArray = [
+  { key: AnimalColorEnum.BEIGE, value: AnimalColorEnum.BEIGE },
+  { key: AnimalColorEnum.BLANC, value: AnimalColorEnum.BLANC },
+  { key: AnimalColorEnum.BLEU, value: AnimalColorEnum.BLEU },
+  { key: AnimalColorEnum.BRINGE, value: AnimalColorEnum.BRINGE },
+  { key: AnimalColorEnum.CHOCOLAT, value: AnimalColorEnum.CHOCOLAT },
+  { key: AnimalColorEnum.ECAILLE_DE_TORTUE, value: AnimalColorEnum.ECAILLE_DE_TORTUE },
+  { key: AnimalColorEnum.FAUVE, value: AnimalColorEnum.FAUVE },
+  { key: AnimalColorEnum.FAUVE_NOIR, value: AnimalColorEnum.FAUVE_NOIR },
+  { key: AnimalColorEnum.GRIS, value: AnimalColorEnum.GRIS },
+  { key: AnimalColorEnum.GRIS_BLANC, value: AnimalColorEnum.GRIS_BLANC },
+  { key: AnimalColorEnum.MARRON, value: AnimalColorEnum.MARRON },
+  { key: AnimalColorEnum.MARRON_BLANC, value: AnimalColorEnum.MARRON_BLANC },
+  { key: AnimalColorEnum.NOIR, value: AnimalColorEnum.NOIR },
+  { key: AnimalColorEnum.ROUX, value: AnimalColorEnum.ROUX },
+  { key: AnimalColorEnum.ROUX_BLANC, value: AnimalColorEnum.ROUX_BLANC },
+  { key: AnimalColorEnum.TIGRE, value: AnimalColorEnum.TIGRE },
+  { key: AnimalColorEnum.TIBRE_BLANC, value: AnimalColorEnum.TIBRE_BLANC },
+  { key: AnimalColorEnum.TIGRE_ROUX, value: AnimalColorEnum.TIGRE_ROUX },
+  { key: AnimalColorEnum.TRICOLORE, value: AnimalColorEnum.TRICOLORE },
+  { key: AnimalColorEnum.TYPE_SIAMOIS, value: AnimalColorEnum.TYPE_SIAMOIS },
+  { key: AnimalColorEnum.NOIR_BLANC, value: AnimalColorEnum.NOIR_BLANC },
+  { key: AnimalColorEnum.CREME, value: AnimalColorEnum.CREME },
+]

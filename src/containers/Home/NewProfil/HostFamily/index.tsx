a22b0589beb98cd53e4756f2b95e77f67a44e555
Edwina <edwina.tan@hotmail.fr>
2023-04-14 15:32:06 +0200

    Lib airtable

--- src/containers/Home/NewProfil/HostFamily/index.tsx
+++ src/containers/Home/NewProfil/HostFamily/index.tsx
@@ -2,15 +2,15 @@ import { useNavigation } from '@react-navigation/native'
 import Airtable from 'airtable'
 import { AIRTABLE_API_KEY, AIRTABLE_APP_ID } from 'config'
 import { useEffect, useState } from 'react'
-import { FlatList, Text } from 'react-native'
+import { Text } from 'react-native'
+import { FlatList } from 'react-native-gesture-handler'
 import { HeaderComponent } from 'src/components/Header'
 import { Layout } from 'src/components/Layout'
 import { AnimalType } from 'src/types/Animal/Type'
 
 export const AddHostFamily = () => {
   const navigation = useNavigation()
-  const [data, setData] = useState<AnimalType[]>()
-  console.log('data', data)
+  const [data, setData] = useState<AnimalType[]>([])
 
   const onClickGoBack = () => {
     return navigation.goBack()
@@ -22,10 +22,12 @@ export const AddHostFamily = () => {
       .select()
       .eachPage(
         function page(records: any, fetchNextPage) {
-          records.forEach(function (record) {
-            setData(records.fields)
-            console.log('get all animal', record.fields)
-          })
+          if (records) {
+            records.forEach(function (record: any) {
+              console.log('data', record.fields)
+              setData((prevState) => [...prevState, record.fields])
+            })
+          }
 
           fetchNextPage()
         },
@@ -37,16 +39,21 @@ export const AddHostFamily = () => {
         }
       )
   }, [])
+  // console.log('data', data)
 
   return (
     <Layout>
       <HeaderComponent onClickGoBack={onClickGoBack} title="Ajouter une famille d’accueil" />
       <Text>ADD HOST FAMILY</Text>
-      <FlatList
-        data={data}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Text>{item.alias}</Text>}
-      />
+      {data ? (
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Text>{item.alias}</Text>}
+        />
+      ) : (
+        <Text>Loading</Text>
+      )}
     </Layout>
   )
 }

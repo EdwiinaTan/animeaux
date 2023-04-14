cc93a1c98ac239944619cc9f373b717344e9f54c
Edwina <edwina.tan@hotmail.fr>
2023-04-14 16:18:20 +0200

    Airtable hooks get animal

--- src/containers/Home/NewProfil/HostFamily/index.tsx
+++ src/containers/Home/NewProfil/HostFamily/index.tsx
@@ -1,52 +1,24 @@
 import { useNavigation } from '@react-navigation/native'
-import Airtable from 'airtable'
-import { AIRTABLE_API_KEY, AIRTABLE_APP_ID } from 'config'
-import { useEffect, useState } from 'react'
 import { Text } from 'react-native'
 import { FlatList } from 'react-native-gesture-handler'
 import { HeaderComponent } from 'src/components/Header'
 import { Layout } from 'src/components/Layout'
-import { AnimalType } from 'src/types/Animal/Type'
+import { useAirtableAnimal } from 'src/hooks/Animal'
 
 export const AddHostFamily = () => {
   const navigation = useNavigation()
-  const [data, setData] = useState<AnimalType[]>([])
+  const { dataTest } = useAirtableAnimal()
 
   const onClickGoBack = () => {
     return navigation.goBack()
   }
-  const base = new Airtable({ apiKey: AIRTABLE_API_KEY }).base(AIRTABLE_APP_ID)
-
-  useEffect(() => {
-    base('animal')
-      .select()
-      .eachPage(
-        function page(records: any, fetchNextPage) {
-          if (records) {
-            records.forEach(function (record: any) {
-              console.log('data', record.fields)
-              setData((prevState) => [...prevState, record.fields])
-            })
-          }
-
-          fetchNextPage()
-        },
-        function done(err) {
-          if (err) {
-            console.error(err)
-            return
-          }
-        }
-      )
-  }, [])
-  // console.log('data', data)
 
   return (
     <Layout>
       <HeaderComponent onClickGoBack={onClickGoBack} title="Ajouter une famille d’accueil" />
       <Text>ADD HOST FAMILY</Text>
       <FlatList
-        data={data}
+        data={dataTest}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => <Text>{item.name}</Text>}
       />

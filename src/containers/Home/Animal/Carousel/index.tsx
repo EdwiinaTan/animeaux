e026cac91daf863b9cac0c44fba2086aa3a9172d
Edwina <edwina.tan@hotmail.fr>
2023-04-16 13:42:08 +0200

    @type/react version17

--- src/containers/Home/Animal/Carousel/index.tsx
+++ src/containers/Home/Animal/Carousel/index.tsx
@@ -1,14 +1,10 @@
 import { useRef, useState } from 'react'
-import { ActivityIndicator, Animated, StyleSheet, useWindowDimensions, View } from 'react-native'
+import { ActivityIndicator, Animated, useWindowDimensions, View } from 'react-native'
 import { Image } from 'react-native-elements'
 import { FlatList } from 'react-native-gesture-handler'
 import { theme } from 'src/constant/Theme'
-import { AnimalType } from 'src/types/Animal/Type'
-import { Carousel, ContainerPagination, PaginationText } from './Styled'
-
-interface CarouselAnimalProps {
-  animal: AnimalType
-}
+import { Carousel, ContainerPagination, PaginationText, style } from './Styled'
+import { CarouselAnimalProps } from './Type'
 
 export const CarouselAnimal: React.FC<CarouselAnimalProps> = ({ animal }) => {
   const { width } = useWindowDimensions()
@@ -17,14 +13,6 @@ export const CarouselAnimal: React.FC<CarouselAnimalProps> = ({ animal }) => {
   const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current
   const [currentIndex, setCurrentIndex] = useState(0)
   const slidesRef = useRef(null)
-
-  const style = StyleSheet.create({
-    container: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-  })
 
   const viewableItemsChanged = useRef(({ viewableItems }) => {
     setCurrentIndex(viewableItems[0].index)

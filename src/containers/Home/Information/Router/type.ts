04ef3352841481193325f0b3194568ba2d8256bb
Edwina <edwina.tan@hotmail.fr>
2023-04-22 12:08:04 +0200

    Information router

---
+++ src/containers/Home/Information/Router/type.ts
@@ -0,0 +1,8 @@
+import { HostFamilyType } from './../../../../types/HostFamily/Type'
+
+export type InformationRouteParams = {
+  informationScreen: undefined
+  informationForm: {
+    informationDetails: HostFamilyType
+  }
+}

50044bf4dcde6e2513d849eb4fd859e1f557d529
Edwina <edwina.tan@hotmail.fr>
2023-04-25 16:27:30 +0200

    Add component login

--- src/components/Form/HostFamily/Utils.tsx
+++ src/components/Form/HostFamily/Utils.tsx
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 export const validationHostFamily = Yup.object().shape({
   lastName: Yup.string().required('Le nom de famille est requis'),
   firstName: Yup.string().required('Le prénom est requis'),
-  email: Yup.string().required('L’adresse mail est requise'),
+  email: Yup.string().email('Le format est incorrect').required('L’adresse mail est requise'),
   phone: Yup.string().required('Le téléphone est requis'),
   postalCode: Yup.string()
     .min(5, 'Le code postal doit contenir au moins 5 numéros')
---
+++ src/containers/Login/index.tsx
@@ -0,0 +1,91 @@
+import { Field, Formik } from 'formik'
+import { Image, TextInput, View } from 'react-native'
+import { Button } from 'react-native-elements'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { styles } from 'src/components/Form/Animal/Styled'
+import { Spacing } from 'src/components/Layout/Spacing'
+import { Body2, Title1 } from 'src/components/Typo'
+import { CardStyle, ContainerStyle, TextRed } from 'src/constant/Theme/Styled'
+import * as Yup from 'yup'
+
+export const Login = () => {
+  const initialValues = {
+    email: '',
+    password: '',
+  }
+
+  const validationAnimalSituation = Yup.object().shape({
+    email: Yup.string().email('Le format est incorrect').required('L’adresse mail est requise'),
+    password: Yup.string().required('Le responsable est obligatoire'),
+  })
+
+  const updateAnimal = async (values) => {
+    console.log('lala')
+  }
+
+  return (
+    <SafeAreaView>
+      <ContainerStyle>
+        <CardStyle>
+          <Spacing size="32" />
+          <Title1 textAlign="center">Bienvenue !</Title1>
+          <Spacing size="24" />
+          <View style={{ alignItems: 'center' }}>
+            <Image source={require('/assets/images/animal.png')} />
+          </View>
+          <Spacing size="32" />
+          <Formik
+            initialValues={initialValues}
+            validationSchema={validationAnimalSituation}
+            onSubmit={(values) => updateAnimal(values)}
+          >
+            {({ handleChange, handleBlur, values, handleSubmit }) => (
+              <>
+                <Body2>
+                  Adresse mail<TextRed>*</TextRed>
+                </Body2>
+                <Spacing size="4" />
+                <Field name="email">
+                  {({ field }) => (
+                    <TextInput
+                      {...field}
+                      style={styles.input}
+                      keyboardType="email-address"
+                      placeholder="Veuillez mettre l’adresse mail"
+                      onChangeText={handleChange('postalCode')}
+                      onChange={handleChange('email')}
+                      onBlur={handleBlur('email')}
+                      value={values.email}
+                    />
+                  )}
+                </Field>
+                <Spacing size="8" />
+                <Body2>
+                  Mot de passe<TextRed>*</TextRed>
+                </Body2>
+                <Spacing size="4" />
+                <Field name="phone">
+                  {({ field }) => (
+                    <TextInput
+                      {...field}
+                      style={styles.input}
+                      placeholder="Veuillez mettre le mot de passe"
+                      onChangeText={handleChange('password')}
+                      onChange={handleChange('password')}
+                      onBlur={handleBlur('password')}
+                      value={values.password}
+                    />
+                  )}
+                </Field>
+                <Spacing size="32" />
+                <Button title="Créer un compte" onPress={() => handleSubmit()} />
+                <Spacing size="16" />
+                <Button title="Connexion" onPress={() => handleSubmit()} />
+              </>
+            )}
+          </Formik>
+        </CardStyle>
+      </ContainerStyle>
+    </SafeAreaView>
+  )
+}

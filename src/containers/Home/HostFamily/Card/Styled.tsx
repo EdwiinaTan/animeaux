706c00fb97110e064031162e13253bd33e51e9c6
Edwina <edwina.tan@hotmail.fr>
2023-04-26 16:28:25 +0200

    Fix hostFamily click

--- src/containers/Home/HostFamily/Card/Styled.tsx
+++ src/containers/Home/HostFamily/Card/Styled.tsx
@@ -4,28 +4,11 @@ export const Container = styled.View`
   display: flex;
   flex-direction: row;
 `
-export const AnimalGender = styled.View`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`
 
 export const ContainerImage = styled.View`
   padding-right: 16px;
 `
 
-export const ContainerChip = styled.View`
-  position: absolute;
-  top: 0;
-  right: 0;
-`
-
 export const Description = styled.View`
-  flex: 1;
-`
-
-export const Image = styled.Image`
-  width: 100px;
-  height: 100px;
-  border-radius: 8px;
+  flex-direction: column;
 `

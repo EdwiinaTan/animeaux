5c10f0f4477cc8baac403623cc508818b5e2fff0
Edwina <edwina.tan@hotmail.fr>
2023-05-01 11:56:04 +0200

    Update color chart

--- src/components/Chart/Utils.tsx
+++ src/components/Chart/Utils.tsx
@@ -1,14 +1,13 @@
 import { theme } from 'src/constant/Theme'
 
 export const chartConfig = {
-  backgroundColor: '#e26a00',
-  backgroundGradientFrom: '#fb8c00',
-  backgroundGradientTo: '#ffa726',
+  backgroundGradientFrom: theme.colors.orange,
+  backgroundGradientTo: theme.colors.yellow,
   decimalPlaces: 0,
   color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
   labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
   props: {
-    strokeWidth: 0,
+    strokeWidth: 2,
     r: '10',
   },
   propsForDots: {
@@ -40,8 +39,8 @@ export const dataBarChart = (label: string[], data: number[]) => {
 
 export const colors = [
   theme.colors.primary,
-  theme.colors.red,
   theme.colors.blue,
+  theme.colors.red,
   theme.colors.yellow,
   theme.colors.secondary,
   theme.colors.grey4,
